1bd4624b45c39e5668936f4b3bf53f6f472fdffb
flipdd <flip.dd@hotmail.com>
2020-02-24 20:15:20 +0000

    ui changes

--- COMP3304Application/Form1.Designer.cs
+++ COMP3304Application/Form1.Designer.cs
@@ -37,44 +37,61 @@
             // 
             // btnPrevious
             // 
-            this.btnPrevious.Location = new System.Drawing.Point(121, 337);
-            this.btnPrevious.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPrevious.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPrevious.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnPrevious.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F);
+            this.btnPrevious.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPrevious.Location = new System.Drawing.Point(122, 320);
+            this.btnPrevious.Margin = new System.Windows.Forms.Padding(2);
             this.btnPrevious.Name = "btnPrevious";
-            this.btnPrevious.Size = new System.Drawing.Size(56, 19);
+            this.btnPrevious.Size = new System.Drawing.Size(80, 40);
             this.btnPrevious.TabIndex = 0;
             this.btnPrevious.Text = "<";
-            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.UseVisualStyleBackColor = false;
             this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(266, 337);
-            this.btnLoad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLoad.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnLoad.AutoSize = true;
+            this.btnLoad.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnLoad.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnLoad.Location = new System.Drawing.Point(260, 320);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(2);
             this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(56, 19);
+            this.btnLoad.Size = new System.Drawing.Size(80, 40);
             this.btnLoad.TabIndex = 1;
             this.btnLoad.Text = "Load";
-            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.UseVisualStyleBackColor = false;
             this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
             // 
             // btnNext
             // 
-            this.btnNext.Location = new System.Drawing.Point(402, 337);
-            this.btnNext.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNext.AutoSize = true;
+            this.btnNext.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnNext.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F);
+            this.btnNext.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnNext.Location = new System.Drawing.Point(391, 320);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(2);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(56, 19);
+            this.btnNext.Size = new System.Drawing.Size(80, 40);
             this.btnNext.TabIndex = 2;
             this.btnNext.Text = ">";
-            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.UseVisualStyleBackColor = false;
             this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
             // 
             // pbImage
             // 
+            this.pbImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pbImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pbImage.Location = new System.Drawing.Point(173, 20);
-            this.pbImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pbImage.Location = new System.Drawing.Point(61, 20);
+            this.pbImage.Margin = new System.Windows.Forms.Padding(2);
             this.pbImage.Name = "pbImage";
-            this.pbImage.Size = new System.Drawing.Size(246, 288);
+            this.pbImage.Size = new System.Drawing.Size(474, 288);
             this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pbImage.TabIndex = 3;
             this.pbImage.TabStop = false;
@@ -82,19 +99,20 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.pbImage);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.btnLoad);
             this.Controls.Add(this.btnPrevious);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
